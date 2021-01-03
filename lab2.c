440f4d16854d4163fb81244bff344892d40742f8
Henipiter <henipiter@wp.pl>
2021-01-03 17:27:59 +0100

    first update

--- lab2.c
+++ lab2.c
@@ -12,16 +12,59 @@ int main(int argc, char** argv){
     struct sockaddr_in s; 
     struct hostent* addrent;
     char buffer[1024];
+    char mess[1024] = { ' ' };
     addrent = gethostbyname(argv[1]);
-    int rc;
+    
     int fd = socket(AF_INET, SOCK_STREAM,0);
     s.sin_family = AF_INET;
     s.sin_port = htons(atoi(argv[2]));
     memcpy(&s.sin_addr.s_addr, addrent->h_addr, addrent->h_length);
-    
+    strcpy(buffer, " ");
     connect(fd, (struct sockaddr*)&s, sizeof(s));
-    write(fd, "1\n4\nsupp", 15);
     
+    write(fd, "1\njeden\n", 8);
+    
+    write(fd, "1\n1\nA", 6);
+    int newline=0;
+    int i=0, counter=0;
+    
+    //historia
+    while(newline!=1){
+        strcpy(buffer, " ");
+        counter=read(fd, buffer, 1);
+        //printf("-> %c\n", buffer[0]);
+        printf(">>> %c\n", buffer[0]);
+        mess[i] = buffer[0];
+        
+        if(mess[i-1] == '\n' && mess[i] == '\n'){
+            newline=1;
+        }
+        i++;
+    }
+    
+    printf("----> %s\n", mess);
+    strcpy(buffer, " ");
+    write(fd, "1\n1\nA", 6);
+    
+    newline=0;
+    i=0; counter=0;
+    
+    //historia
+    while(newline!=1){
+        strcpy(buffer, " ");
+        counter=read(fd, buffer, 1);
+        //printf("-> %c\n", buffer[0]);
+        printf(">>> %c\n", buffer[0]);
+        mess[i] = buffer[0];
+        
+        if(mess[i-1] == '\n' && mess[i] == '\n'){
+            newline=1;
+        }
+        i++;
+    }
+    
+    printf("----> %s\n", mess);
+    strcpy(buffer, " ");
     close(fd);
     return EXIT_SUCCESS;
 }
--- serverProjekt.c
+++ serverProjekt.c
@@ -15,53 +15,358 @@ struct cln {
     int cfd;
     struct sockaddr_in caddr;
 };
+
+struct user_data{
+    char* name;
+    int logged;
+    char* password;
+    int index_forum; //forum do ktorego nalezy uzytkownik. Potrzebne przy zmianie forum
+};
+struct forum_data{
+    int id_users[9];
+    char *username[9];
+    int countUser;
+    char* name;
+};
+
+
+int set=0;
+
+size_t len=0;
+char historyForum[10][11] = { "forum0.txt", "forum1.txt", "forum2.txt", "forum3.txt", "forum4.txt", "forum5.txt", "forum6.txt", "forum7.txt", "forum8.txt", "forum9.txt" };
+struct user_data users[9];
+struct forum_data forums[10];
+
+void showForum(){
+    for(int i=0;i<10;i++){
+        printf("%d: %s %d\n", i, forums[i].name, forums[i].countUser); 
+    }
+}
+
+void setup(){
+    FILE *fileUser=fopen("fileUser.txt", "rt");
+    FILE *fileForum=fopen("fileForum.txt", "rt");
+    set=1;
+    len=0;
+    for( int i=0;i<9;i++){
+        users[i].logged = 0;
+        getline(&users[i].name, &len, fileUser);
+        strtok(users[i].name, "\n"); 
+        users[i].index_forum = -1;
+    }
+    for( int i=0;i<9;i++){
+        getline(&users[i].password, &len, fileUser);
+        strtok(users[i].password, "\n"); 
+    }
+    for( int i=0;i<10;i++){
+        forums[i].countUser = 0;
+        
+        for(int j=0; j<9;j++){
+            forums[i].id_users[j]=0;
+            forums[i].username[j] ="w";
+        }
+        
+        getline(&forums[i].name, &len, fileForum);
+        strtok(forums[i].name, "\n");
+    }
+    fclose(fileUser);
+    fclose(fileForum);
+}
+void leaveForum(int index_user, int cfd){
+    for(int j=0; j<9;j++){  
+        if( forums[users[index_user].index_forum].id_users[j] == cfd ){
+            forums[users[index_user].index_forum].id_users[j] = forums[users[index_user].index_forum].id_users[forums[users[index_user].index_forum].countUser];
+            forums[users[index_user].index_forum].id_users[forums[users[index_user].index_forum].countUser] = 0;
+            
+            
+            forums[users[index_user].index_forum].username[j] = forums[users[index_user].index_forum].username[forums[users[index_user].index_forum].countUser];
+            forums[users[index_user].index_forum].username[forums[users[index_user].index_forum].countUser] = "w";
+            
+            
+            j=9; 
+        }
+    }
+}
+
+
+
+int compare(char* str1, char* str2){
+    int i=0;
+    while(str1[i]!='\0' && str2[i]!='\0'){
+        if(str1[i]!=str2[i]){
+            return 0;
+        }
+        else if(str1[i]=='\0'){  
+            return 1;
+        }
+        i++;
+    }
+    if(str1[i]!=str2[i]){
+        return 0;
+    } 
+    return 1;
+}
+
+
 void* cthread(void* arg){
     char buffer[1024];
+    char login[256] = {'w'};
+    char password[256] = {'w'};
     int counter = 0;
+    int written = 0;
+    int index_user;
     int countNewLine = 0;
     int logged;
     char flag;
     int size=0;
     int i=0;
-    logged = 1;
+    int password_iter, login_iter;
+    logged = 0;
     //funkcja logowania
     struct cln* c = (struct cln*)arg;
-    
-    while(logged){
+    while(logged==0){
         while(countNewLine <2){
             read(c->cfd, buffer, 1);
-             printf("tekst: %s\n", buffer);
+            //printf("tekstf: %d %d %s %s %c\n",countNewLine,w, login, password, buffer[0]);
+            if (countNewLine == 0 && buffer[0] != '\n'){
+                login[login_iter] = buffer[0];
+                login_iter++;
+            }
+            if (countNewLine == 1 && buffer[0] != '\n'){
+                password[password_iter] = buffer[0];
+                password_iter++;
+            }
+            if ( buffer[0] == '\n'){
+                countNewLine++;
+            } 
+        }
+        for( int i=0;i<9;i++){
+            if(compare(login, users[i].name)){
+                if(compare(password, users[i].password)){
+                    users[i].logged = 1;
+                    logged=1;
+                    index_user = i;
+                    printf("zalogowano\n");
+                }
+            }
+        }
+        if(logged==0){
+            printf("blad logowania\n");
+        }
+    }
+    
+    while(logged==1){
+        countNewLine=0;
+        while(countNewLine <2){
+            read(c->cfd, buffer, 1);
+            printf("-%c\n", buffer[0]);
+            if ( buffer[0] == '\n'){
+                countNewLine++;
+            }
             if( countNewLine == 0){
+                printf("dd\n");
                 flag = buffer[0];
             }
             if (countNewLine != 0 && buffer[0] != '\n'){
                 size = size * 10 + buffer[0] - '0';
             }
-            if ( buffer[0] == '\n'){
-                countNewLine++;
-            }
             
         }
-         printf("size %d\n", size);
         countNewLine=0;
         while(i < size){
-        
             counter = read(c->cfd, buffer+i, 1024);
-            printf("teksttt: %s %d %d %d\n", buffer, counter, i, size);
+            printf("Bufor: %c %s %d\n", flag, buffer, size);
             i+= counter;
         }
         i=0;
-        printf("tekst: %s\n", buffer);
-        logged = 0;
+        size=0;
+        // flag - typ komunikatu
+        //buffer - tresc
+        
+        /*
+         * 1 - wybierz(dolacz) forum 
+         * 2 - napisz na forum
+         * 3 - wyloguj
+         * 4 - utworz forum
+         * 5 - usun forum
+         * 6 - napisz do uzytkownika
+         * 7 - zwroc liste forum
+         * 8 - zwroc liste zalogowanych na forum uzytkownikow
+         * 
+        */
+        switch( flag ){
+            case '1':
+                printf("Opcja1\n");
+                for(int i=0;i<10;i++){
+                    if(forums[i].name[0] != '-'){
+                        if(compare( forums[i].name, buffer)){
+                            forums[i].id_users[ forums[i].countUser] = c->cfd;
+                            forums[i].countUser++;
+                            //jeżeli użytkownik jest w jakimś forum
+                            if(users[index_user].index_forum != -1){
+                                forums[users[index_user].index_forum].countUser--;
+                                //usun uzytkownika z biezacego forum
+                                
+                                leaveForum(index_user, c->cfd);
+                                
+                                forums[i].id_users[forums[i].countUser] = c->cfd;
+                                forums[i].username[forums[i].countUser] = users[index_user].name;
+                            }
+                            users[index_user].index_forum = i;
+                            i=10;
+                            
+                            /*
+                             * TO DO
+                             */
+                            //odczytaj plik forum
+                            char* historyMessege;
+                            FILE *hist = fopen( historyForum[ users[index_user].index_forum], "rt");
+                            int cfdd;
+                            printf("wysylanie...\n");
+                            while(getline(&historyMessege, &len, hist) >0){
+                                cfdd = c->cfd;
+                                counter=0;
+                                printf("%s %ld\n", historyMessege, strlen(historyMessege) );
+                                while( counter<strlen(historyMessege)){
+                                    written = write(cfdd,historyMessege, strlen(historyMessege));  
+                                    counter+=written;
+                                }
+                            }
+                            printf("Wyslano\n");
+                            write(cfdd, "\n\n",2); 
+                            fclose(hist);
+                            
+                        }
+                    }
+                }
+                
+                showForum();
+                break;
+            case '2':
+                printf("Opcja2\n");
+                for(int i=0;i<forums[ users[index_user].index_forum].countUser;i++){
+                    strcat(buffer, "\n");
+                    counter = 0;
+                    written = 0;
+                    int cfdd = forums[ users[index_user].index_forum ].id_users[i];
+                    while( counter<size){
+                        written = write(cfdd,buffer, size);  
+                        counter+=written;
+                    }
+                    write(cfdd, "\n", 1);
+                    FILE *history = fopen( historyForum[users[index_user].index_forum], "rt");
+                    fprintf(history, "%s\n", buffer);
+                    fclose(history);
+                }
+                break;
+                
+            case '3':
+                printf("Opcja3\n");
+                users[index_user].logged =0;
+                logged = 0;
+                leaveForum(index_user, c->cfd);
+                break;
+            case '4':
+                printf("Opcja4\n");
+                for(int i=0;i<10;i++){
+                    if(forums[i].name[0] == '-'){
+                        FILE *history = fopen( historyForum[i], "rt");
+                        i=10;
+                        strcpy( forums[i].name, buffer);
+                        fprintf(history, "%s\n", buffer);
+                        fclose(history);
+                    }
+                }
+                break;
+            case '5':
+                printf("Opcja5\n");
+                for(int i=0;i<10;i++){
+                    if(compare( forums[i].name, buffer)){
+                        counter = 0;
+                        written = 0;
+                        FILE *history = fopen( historyForum[i], "w");
+                        strcpy( forums[i].name, "-");
+                        for(int j=0;j<forums[i].countUser;j++){
+                            int cfdd = forums[i].id_users[j];
+                            while( counter<15){
+                                written = write(cfdd,"Forum usunieto\n", 15);  
+                                counter+=written;
+                            }
+                            forums[i].id_users[j] = 0;
+                        }
+                        fclose(history);
+                    }
+                    for(int j=0;j<9;j++){
+                        if( users[j].index_forum == i ){
+                            users[j].index_forum = -1;   
+                        }
+                    }
+                    i=10;
+                }
+                break;
+            case '6':
+                printf("Opcja6\n");
+                i=0;
+                char* send_to;
+                char* mess;
+                counter = 0;
+                written = 0;
+                send_to = strtok( buffer, "\t");
+                mess = strtok(NULL, "\t");
+                for(int j=0; j< forums[index_user].countUser;j++){
+                    if( compare( send_to, forums[index_user].username[j])){
+                        int cfdd = forums[index_user].id_users[j];
+                        while( counter<strlen(mess)){
+                            written = write(cfdd,mess, strlen(mess));  
+                            counter+=written;
+                        }
+                        write(cfdd, "\n", 1);
+                    }
+                }
+                break;
+            case '7':
+                printf("Opcja7\n");
+                counter = 0;
+                written = 0;
+                for( int i=0;i<10; i++){
+                    counter = 0;
+                    written = 0;
+                    if( forums[i].name[0] != '-' ){
+                        while( counter<strlen(mess)){
+                            written = write(c->cfd ,forums[i].name, strlen(forums[i].name));  
+                            counter+=written;
+                        }
+                        write(c->cfd ,"\t", 1);
+                    }
+                }
+                write(c->cfd ,"\n", 1);
+                break;
+            case '8':
+                printf("Opcja8\n");
+                for(int i=0;i<forums[index_user].countUser;i++){
+                    counter = 0;
+                    written = 0;
+                    while( counter<strlen(mess)){
+                        written = write(c->cfd ,forums[i].name, strlen(forums[i].name));  
+                        counter+=written;
+                    }
+                    write(c->cfd ,"\t", 1);
+                }
+                write(c->cfd ,"\n", 1);
+                
+                break;
+        }
     }
     close(c->cfd);
     free(c);
     return EXIT_SUCCESS;
 }
 int main(int argc, char** argv){
+    if(set==0)
+        setup();
+    
     pthread_t tid;
     socklen_t slt;
-    
     int sfd, on=1;
     struct sockaddr_in saddr, caddr;
     saddr.sin_family = AF_INET;
