f468f5296998b056e6fae7d68a438e9ca2e447f4
Henipiter <68025378+Henipiter@users.noreply.github.com>
2021-01-04 21:07:52 +0100

    GUI

---
+++ DayTime/DayTime/Form1.Designer.cs
@@ -0,0 +1,221 @@
+namespace DayTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxIndex = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.buttonGetDate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxIndex);
+            this.groupBox1.Controls.Add(this.textBoxPassword);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxLogin);
+            this.groupBox1.Controls.Add(this.buttonGetDate);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(483, 146);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server Info";
+            // 
+            // textBoxIndex
+            // 
+            this.textBoxIndex.Location = new System.Drawing.Point(211, 92);
+            this.textBoxIndex.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxIndex.Name = "textBoxIndex";
+            this.textBoxIndex.Size = new System.Drawing.Size(132, 22);
+            this.textBoxIndex.TabIndex = 2;
+            this.textBoxIndex.Text = "141298";
+            this.textBoxIndex.TextChanged += new System.EventHandler(this.textBoxPort_TextChanged_1);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(211, 60);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(132, 22);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.Text = "jeden";
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPort_TextChanged_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 92);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Index:";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(211, 23);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(263, 22);
+            this.textBoxLogin.TabIndex = 1;
+            this.textBoxLogin.Text = "1";
+            // 
+            // buttonGetDate
+            // 
+            this.buttonGetDate.Location = new System.Drawing.Point(8, 111);
+            this.buttonGetDate.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonGetDate.Name = "buttonGetDate";
+            this.buttonGetDate.Size = new System.Drawing.Size(100, 28);
+            this.buttonGetDate.TabIndex = 3;
+            this.buttonGetDate.Text = "Run";
+            this.buttonGetDate.UseVisualStyleBackColor = true;
+            this.buttonGetDate.Click += new System.EventHandler(this.buttonGetDate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxDate);
+            this.groupBox2.Location = new System.Drawing.Point(23, 169);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(479, 78);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxDate.ForeColor = System.Drawing.Color.Black;
+            this.textBoxDate.Location = new System.Drawing.Point(9, 34);
+            this.textBoxDate.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.ReadOnly = true;
+            this.textBoxDate.Size = new System.Drawing.Size(461, 23);
+            this.textBoxDate.TabIndex = 0;
+            this.textBoxDate.TabStop = false;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 248);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            this.statusStrip.Size = new System.Drawing.Size(513, 22);
+            this.statusStrip.TabIndex = 0;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 16);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 270);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(531, 317);
+            this.Name = "Form1";
+            this.Text = "TCP DayTime Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonGetDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIndex;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DayTime/DayTime/UserControl1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace DayTime
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CanalBox = new System.Windows.Forms.GroupBox();
+            this.UserBox = new System.Windows.Forms.GroupBox();
+            this.ChatBox = new System.Windows.Forms.GroupBox();
+            this.UserWriteButton = new System.Windows.Forms.Button();
+            this.UserRefreshButton = new System.Windows.Forms.Button();
+            this.ForumRefreshButton = new System.Windows.Forms.Button();
+            this.ForumAddButton = new System.Windows.Forms.Button();
+            this.ForumDeleteButton = new System.Windows.Forms.Button();
+            this.ChatSendButton = new System.Windows.Forms.Button();
+            this.ChatWriteBox = new System.Windows.Forms.TextBox();
+            this.ForumListBox = new System.Windows.Forms.TextBox();
+            this.UserListBox = new System.Windows.Forms.TextBox();
+            this.ChatHistoryBox = new System.Windows.Forms.TextBox();
+            this.LogoutButton = new System.Windows.Forms.Button();
+            this.CanalBox.SuspendLayout();
+            this.UserBox.SuspendLayout();
+            this.ChatBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CanalBox
+            // 
+            this.CanalBox.Controls.Add(this.ForumListBox);
+            this.CanalBox.Controls.Add(this.ForumDeleteButton);
+            this.CanalBox.Controls.Add(this.ForumAddButton);
+            this.CanalBox.Controls.Add(this.ForumRefreshButton);
+            this.CanalBox.Location = new System.Drawing.Point(39, 24);
+            this.CanalBox.Name = "CanalBox";
+            this.CanalBox.Size = new System.Drawing.Size(185, 266);
+            this.CanalBox.TabIndex = 0;
+            this.CanalBox.TabStop = false;
+            this.CanalBox.Text = "Lista kanalow";
+            // 
+            // UserBox
+            // 
+            this.UserBox.Controls.Add(this.UserListBox);
+            this.UserBox.Controls.Add(this.UserRefreshButton);
+            this.UserBox.Controls.Add(this.UserWriteButton);
+            this.UserBox.Location = new System.Drawing.Point(39, 296);
+            this.UserBox.Name = "UserBox";
+            this.UserBox.Size = new System.Drawing.Size(185, 266);
+            this.UserBox.TabIndex = 0;
+            this.UserBox.TabStop = false;
+            this.UserBox.Text = "Dostepni uzytkownicy";
+            // 
+            // ChatBox
+            // 
+            this.ChatBox.Controls.Add(this.ChatHistoryBox);
+            this.ChatBox.Controls.Add(this.ChatWriteBox);
+            this.ChatBox.Controls.Add(this.ChatSendButton);
+            this.ChatBox.Controls.Add(this.LogoutButton);
+            this.ChatBox.Location = new System.Drawing.Point(249, 24);
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(771, 538);
+            this.ChatBox.TabIndex = 0;
+            this.ChatBox.TabStop = false;
+            this.ChatBox.Text = "Czat";
+            // 
+            // UserWriteButton
+            // 
+            this.UserWriteButton.Location = new System.Drawing.Point(19, 218);
+            this.UserWriteButton.Name = "UserWriteButton";
+            this.UserWriteButton.Size = new System.Drawing.Size(143, 28);
+            this.UserWriteButton.TabIndex = 0;
+            this.UserWriteButton.Text = "Napisz";
+            this.UserWriteButton.UseVisualStyleBackColor = true;
+            // 
+            // UserRefreshButton
+            // 
+            this.UserRefreshButton.Location = new System.Drawing.Point(19, 184);
+            this.UserRefreshButton.Name = "UserRefreshButton";
+            this.UserRefreshButton.Size = new System.Drawing.Size(143, 28);
+            this.UserRefreshButton.TabIndex = 0;
+            this.UserRefreshButton.Text = "Refresh";
+            this.UserRefreshButton.UseVisualStyleBackColor = true;
+            // 
+            // ForumRefreshButton
+            // 
+            this.ForumRefreshButton.Location = new System.Drawing.Point(19, 162);
+            this.ForumRefreshButton.Name = "ForumRefreshButton";
+            this.ForumRefreshButton.Size = new System.Drawing.Size(143, 28);
+            this.ForumRefreshButton.TabIndex = 0;
+            this.ForumRefreshButton.Text = "Refresh";
+            this.ForumRefreshButton.UseVisualStyleBackColor = true;
+            // 
+            // ForumAddButton
+            // 
+            this.ForumAddButton.Location = new System.Drawing.Point(19, 196);
+            this.ForumAddButton.Name = "ForumAddButton";
+            this.ForumAddButton.Size = new System.Drawing.Size(143, 28);
+            this.ForumAddButton.TabIndex = 0;
+            this.ForumAddButton.Text = "Add";
+            this.ForumAddButton.UseVisualStyleBackColor = true;
+            // 
+            // ForumDeleteButton
+            // 
+            this.ForumDeleteButton.Location = new System.Drawing.Point(19, 230);
+            this.ForumDeleteButton.Name = "ForumDeleteButton";
+            this.ForumDeleteButton.Size = new System.Drawing.Size(143, 28);
+            this.ForumDeleteButton.TabIndex = 0;
+            this.ForumDeleteButton.Text = "Delete";
+            this.ForumDeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // ChatSendButton
+            // 
+            this.ChatSendButton.Location = new System.Drawing.Point(603, 456);
+            this.ChatSendButton.Name = "ChatSendButton";
+            this.ChatSendButton.Size = new System.Drawing.Size(143, 62);
+            this.ChatSendButton.TabIndex = 0;
+            this.ChatSendButton.Text = "Send";
+            this.ChatSendButton.UseVisualStyleBackColor = true;
+            // 
+            // ChatWriteBox
+            // 
+            this.ChatWriteBox.Location = new System.Drawing.Point(23, 456);
+            this.ChatWriteBox.Multiline = true;
+            this.ChatWriteBox.Name = "ChatWriteBox";
+            this.ChatWriteBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ChatWriteBox.Size = new System.Drawing.Size(564, 61);
+            this.ChatWriteBox.TabIndex = 1;
+            // 
+            // ForumListBox
+            // 
+            this.ForumListBox.Location = new System.Drawing.Point(20, 24);
+            this.ForumListBox.Multiline = true;
+            this.ForumListBox.Name = "ForumListBox";
+            this.ForumListBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ForumListBox.Size = new System.Drawing.Size(141, 128);
+            this.ForumListBox.TabIndex = 1;
+            // 
+            // UserListBox
+            // 
+            this.UserListBox.Location = new System.Drawing.Point(20, 31);
+            this.UserListBox.Multiline = true;
+            this.UserListBox.Name = "UserListBox";
+            this.UserListBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.UserListBox.Size = new System.Drawing.Size(141, 141);
+            this.UserListBox.TabIndex = 1;
+            // 
+            // ChatHistoryBox
+            // 
+            this.ChatHistoryBox.Location = new System.Drawing.Point(23, 51);
+            this.ChatHistoryBox.Multiline = true;
+            this.ChatHistoryBox.Name = "ChatHistoryBox";
+            this.ChatHistoryBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ChatHistoryBox.Size = new System.Drawing.Size(722, 392);
+            this.ChatHistoryBox.TabIndex = 2;
+            // 
+            // LogoutButton
+            // 
+            this.LogoutButton.Location = new System.Drawing.Point(602, 17);
+            this.LogoutButton.Name = "LogoutButton";
+            this.LogoutButton.Size = new System.Drawing.Size(143, 28);
+            this.LogoutButton.TabIndex = 0;
+            this.LogoutButton.Text = "Logout";
+            this.LogoutButton.UseVisualStyleBackColor = true;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ChatBox);
+            this.Controls.Add(this.UserBox);
+            this.Controls.Add(this.CanalBox);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1063, 601);
+            this.CanalBox.ResumeLayout(false);
+            this.CanalBox.PerformLayout();
+            this.UserBox.ResumeLayout(false);
+            this.UserBox.PerformLayout();
+            this.ChatBox.ResumeLayout(false);
+            this.ChatBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox CanalBox;
+        private System.Windows.Forms.GroupBox UserBox;
+        private System.Windows.Forms.GroupBox ChatBox;
+        private System.Windows.Forms.Button ForumDeleteButton;
+        private System.Windows.Forms.Button ForumAddButton;
+        private System.Windows.Forms.Button ForumRefreshButton;
+        private System.Windows.Forms.Button UserRefreshButton;
+        private System.Windows.Forms.Button UserWriteButton;
+        private System.Windows.Forms.Button ChatSendButton;
+        private System.Windows.Forms.TextBox ChatWriteBox;
+        private System.Windows.Forms.TextBox ForumListBox;
+        private System.Windows.Forms.TextBox UserListBox;
+        private System.Windows.Forms.TextBox ChatHistoryBox;
+        private System.Windows.Forms.Button LogoutButton;
+    }
+}
